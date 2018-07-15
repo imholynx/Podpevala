2696b2e57cc792ce6507ea5c1c100b00b3d4e238
Ilya Oputin <imholynx@gmail.com>
2018-07-15 21:56:07 +0300

    Change SoundButtonView

--- app/src/main/java/com/imholynx/podpevala/MainActivity.kt
+++ app/src/main/java/com/imholynx/podpevala/MainActivity.kt
@@ -8,156 +8,176 @@ import android.os.Bundle
 import android.os.Environment
 import android.provider.Settings
 import android.support.v4.app.ActivityCompat
-import android.support.v4.app.FragmentManager
 import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.widget.Button
+import android.widget.SeekBar
 import android.widget.Toast
 import com.acrcloud.rec.sdk.ACRCloudClient
 import com.acrcloud.rec.sdk.ACRCloudConfig
 import com.acrcloud.rec.sdk.IACRCloudListener
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 import java.io.File
 
-class MainActivity : AppCompatActivity(),IACRCloudListener,PermissionsDialogFragment.OnFragmentInteractionListener {
-    override fun onFragmentInteraction(uri: Uri) {
+class MainActivity : AppCompatActivity(), IACRCloudListener, ActivityCompat.OnRequestPermissionsResultCallback {
 
+    val MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 101
+
+    lateinit var config: ACRCloudConfig
+    lateinit var client: ACRCloudClient
+
+    var initState: Boolean = false
+    var processing: Boolean = false
+
+    var startTime: Long = 0
+    var stopTime: Long = 0
+
+    override fun onVolumeChanged(vlm: Double) {
+        volume.text = vlm.toString()
+        sound_button.setVolume(vlm.toFloat())
     }
 
-    lateinit var config:ACRCloudConfig
-    lateinit var client:ACRCloudClient
+    override fun onResult(json: String?) {
+        result.text = json
+        cancel()
 
-    var initState:Boolean = false
-    var processing:Boolean = false
-
-    var startTime:Long = 0
-    var stopTime:Long = 0
-
-    override fun onVolumeChanged(p0: Double) {
-        volume.text  = p0.toString()
-    }
-
-    override fun onResult(p0: String?) {
-        result.text = p0
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var path = Environment.getExternalStorageDirectory().toString() + "/acrcloud/model"
+        start.setOnClickListener{ view -> if(processing) stop() else start() }
+        //sound_button.setOnClickListener{ view -> if(processing) stop() else start() }
 
-        val file = File(path)
-        if (!file.exists()) {
-            file.mkdirs()
-        }
+        //TODO удалить
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
 
-        val startBtn: Button = findViewById(R.id.start)
-        val cancelBtn: Button = findViewById(R.id.cancel)
-        val stopBtn: Button = findViewById(R.id.stop)
-        startBtn.setOnClickListener{view -> start(); }
-        cancelBtn.setOnClickListener{view -> cancel(); }
-        stopBtn.setOnClickListener{view -> stop(); }
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                sound_button.setVolume(p1.toFloat()/100)
+            }
+
+        })
 
         config = ACRCloudConfig()
         config.acrcloudListener = this
         config.context = this
         config.host = "identify-eu-west-1.acrcloud.com"
-        config.dbPath = path
         config.accessKey = "4db8d90f77f706fc96be20fafe5e6665"
         config.accessSecret = "nPkCxwyjVLxKAx9rQ0Rx5NAOgp2GbHpQjSfTSfNj"
         config.protocol = ACRCloudConfig.ACRCloudNetworkProtocol.PROTOCOL_HTTPS
         config.reqMode = ACRCloudConfig.ACRCloudRecMode.REC_MODE_REMOTE
 
         client = ACRCloudClient()
-
-        this.initState = client.initWithConfig(config)
+        initState = client.initWithConfig(config)
 
         if (initState) {
-            //TODO переделать
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
-                    == PackageManager.PERMISSION_GRANTED) {
-
+            if (checkPermissionsOrRequest())
                 client.startPreRecord(3000)
-                //Start recognition
-
-            } else if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                            Manifest.permission.RECORD_AUDIO)) {
-
-                val permissionsDialogFragment = PermissionsDialogFragment.newInstance("","")
-                val fragmentManager = getSupportFragmentManager()
-                val fragmentTransaction = fragmentManager.beginTransaction()
-                fragmentTransaction.add(R.id.frameId,permissionsDialogFragment)
-                fragmentTransaction.commit()
-
-                /*val intent = Intent()
-                intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-                val uri = Uri.fromParts("package", packageName, null)
-                intent.data = uri
-                startActivity(intent)*/
-            } else {
-                Log.d(this@MainActivity.localClassName,"ask for permission")
-                makeRequest()
-            }
         }
     }
-
-
 
     private fun makeRequest() {
         ActivityCompat.requestPermissions(this,
                 arrayOf(Manifest.permission.RECORD_AUDIO),
-                101)
+                MY_PERMISSIONS_REQUEST_RECORD_AUDIO)
+        Manifest.permission.RECORD_AUDIO
+    }
+
+    // return true if permission granted
+    fun checkPermissionsOrRequest(): Boolean {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_GRANTED) {
+            return true
+        } else if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.RECORD_AUDIO)) {
+            showExplanationDialog()
+            return false
+        } else {
+            makeRequest()
+            return false
+        }
+    }
+
+    fun showExplanationDialog() {
+        AlertDialog.Builder(this)
+                .setTitle(R.string.micro_unavailable)
+                .setMessage(R.string.message)
+                .setPositiveButton(R.string.open_settings) { _, _ ->
+                    val intent = Intent()
+                    intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                }
+                .setNegativeButton(R.string.cancel) { _, _ -> checkPermissionsOrRequest() }
+                .show()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when(requestCode){
-            101 -> {
-                if(grantResults.isEmpty() || grantResults[0] !=PackageManager.PERMISSION_GRANTED){
-
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_RECORD_AUDIO -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    showExplanationDialog()
+                } else {
+                    client.startPreRecord(3000)
                 }
-                else{
-
-                }
-
             }
         }
     }
 
-    fun start(){
-        if(!initState){
-            Toast.makeText(this,"please wait a second for init",Toast.LENGTH_SHORT).show()
-            return;
-        }
-
-        if(!processing){
-            processing = true
-            if(!::client.isInitialized || !client.startRecognize()){
-                processing = false
-                result.text = "start error"
+    fun start() {
+        if (checkPermissionsOrRequest()) {
+            if (!initState) {
+                Toast.makeText(this, "please wait a second for init", Toast.LENGTH_SHORT).show()
+                return;
             }
-            startTime = System.currentTimeMillis()
+            if (!processing) {
+                processing = true
+                if (!::client.isInitialized || !client.startRecognize()) {
+                    processing = false
+                    result.text = "start error"
+                }
+                startTime = System.currentTimeMillis()
+            }
         }
     }
-    fun cancel(){
-        if(processing && ::client.isInitialized)
-        {
+
+    fun cancel() {
+        if (processing && ::client.isInitialized) {
             processing = false
             client.cancel()
-            result.text = ""
-            tv_time.text = ""
-
-
         }
     }
-    fun stop(){
-        if(processing && ::client.isInitialized){
+
+    fun stop() {
+        if (processing && ::client.isInitialized) {
             client.stopRecordToRecognize()
         }
         processing = false
         stopTime = System.currentTimeMillis()
+    }
+
+    override fun onPause() {
+        if (processing && ::client.isInitialized) {
+            processing = false
+            client.cancel()
+        }
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        if(::client.isInitialized) {
+            client.cancel()
+            client.release()
+        }
+        super.onDestroy()
     }
 
 }
---
+++ app/src/main/java/com/imholynx/podpevala/SoundButtonView.kt
@@ -0,0 +1,91 @@
+package com.imholynx.podpevala
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+
+class SoundButtonView : View {
+
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) :
+            super(context, attrs, defStyleAttr) {
+        init(context, attrs)
+    }
+
+
+    fun init(context: Context, attrs: AttributeSet?) {
+        attrs?.let {
+            val typedArray = context.obtainStyledAttributes(it, R.styleable.SoundButtonView, 0, 0)
+            try {
+                centerColor = typedArray.getColor(R.styleable.SoundButtonView_center_color, Color.BLUE)
+                borderColor = typedArray.getColor(R.styleable.SoundButtonView_border_color, Color.CYAN)
+                borderWidth = typedArray.getDimension(R.styleable.SoundButtonView_border_width, 4f)
+                volume = typedArray.getFloat(R.styleable.SoundButtonView_volume,0f)
+                circleRadius = typedArray.getFloat(R.styleable.SoundButtonView_circle_radius, 0.5f)
+                icon  = typedArray.getDrawable(R.styleable.SoundButtonView_icon)
+                } finally{
+                typedArray.recycle()
+            }
+        }
+    }
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+
+    private var circleRadius = 0.5f
+    private var centerColor = Color.CYAN
+    private var borderColor = Color.BLUE
+    private var borderWidth = 4.0f
+    private var volume:Float = 0.5f
+    private var icon: Drawable? = null
+
+    fun setVolume(value: Float)
+    {
+        volume = value
+        invalidate()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val width = measuredWidth
+        setMeasuredDimension(width, width)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        drawCircle(canvas)
+        drawBorder(canvas)
+        icon!!.draw(canvas)
+    }
+
+    private fun drawCircle(canvas: Canvas) {
+        paint.color = centerColor
+        paint.style = Paint.Style.FILL
+        val size = measuredWidth
+        val radius = (size / 2f)*circleRadius
+        //val radius = size / 2f
+        canvas.drawCircle(size / 2f, size / 2f, radius, paint)
+    }
+
+    private fun drawBorder(canvas: Canvas) {
+        paint.color = borderColor
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = borderWidth
+
+        val size = measuredWidth
+        val min = (circleRadius*size + borderWidth)/2f
+        val max = (size  - borderWidth)/2f
+        val borderRadius = min + volume*(max-min)
+
+        canvas.drawCircle(size / 2f, size / 2f, borderRadius, paint)
+
+
+    }
+}
